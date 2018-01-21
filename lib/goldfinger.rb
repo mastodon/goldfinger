d5f16ebeca5189c0f6aba952866a2dca7aee2a5d
MIYAGI Hikaru <hcmiya@users.noreply.github.com>
2018-01-22 08:11:46 +0900

    Enable to pass options to HTTP.rb, Allow insecure request (#6)
    * Enable to pass options to HTTP.rb, Allow insecure request
    
    * Move SSL option to the hash
    
    * fix a bug

--- lib/goldfinger.rb
+++ lib/goldfinger.rb
@@ -18,8 +18,9 @@ module Goldfinger
   # @raise [Goldfinger::NotFoundError] Error raised when the Webfinger resource could not be retrieved
   # @raise [Goldfinger::SSLError] Error raised when there was a SSL error when fetching the resource
   # @param uri [String] A full resource identifier in the format acct:user@example.com
+  # @param opts [Hash] Options passed to HTTP.rb client
   # @return [Goldfinger::Result]
-  def self.finger(uri)
-    Goldfinger::Client.new(uri).finger
+  def self.finger(uri, opts = {})
+    Goldfinger::Client.new(uri, opts).finger
   end
 end
